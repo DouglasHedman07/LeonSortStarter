5d2266d0f3076680a33d3e6a94c2a00b1bf14661
DouglasHedman07 <52246706+DouglasHedman07@users.noreply.github.com>
2020-03-10 16:55:43 -0600

    xdd

--- src/edu/ujcv/progra1/CycleSort.java
+++ src/edu/ujcv/progra1/CycleSort.java
@@ -5,3 +5,11 @@ public class CycleSort {
     {
         int[] array1 = {12, 11, 15, 10, 9, 1, 2, 3, 13, 14, 4, 5, 6, 7, 8};
         int[] array2 = {2, 1, 5, 2, 9, 1, 2, 3, 3, 4, 4, 5, 6, 7, 8};
+
+        CycleSort sorter = new CycleSort();
+        sorter.cycleSort(array1);
+        sorter.cycleSort(array2);
+
+        System.out.println(java.util.Arrays.toString(array1));
+        System.out.println(java.util.Arrays.toString(array2));
+    }
